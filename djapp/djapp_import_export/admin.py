c7b115a938a8a9630340ddc79de08da77cb0d01b
GeorgeWork1005 <GeorgeWork1005@gmail.com>
2026-01-24 17:02:35 +0800

    finished import

--- djapp/djapp_import_export/admin.py
+++ djapp/djapp_import_export/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import UserHair
+from .models import Coordinate
+from .models import Address
+from .models import Company
+from .models import UserData
+
+admin.site.register(UserHair)
+admin.site.register(Coordinate)
+admin.site.register(Address)
+admin.site.register(Company)
+admin.site.register(UserData)
