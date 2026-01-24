0476590ec8e14eeb371dc5cdc6fd230abae498af
GeorgeWork1005 <GeorgeWork1005@gmail.com>
2026-01-24 21:53:56 +0800

    also fixed delete data modal; starting on export

--- djapp/utils/utils_export.py
+++ djapp/utils/utils_export.py
@@ -1,10 +1,11 @@
 
-from .models import UserHair
-from .models import Coordinate
-from .models import Address
-from .models import Company
-from .models import UserData
+from djapp.djapp_import_export.models import UserHair
+from djapp.djapp_import_export.models import Coordinate
+from djapp.djapp_import_export.models import Address
+from djapp.djapp_import_export.models import Company
+from djapp.djapp_import_export.models import UserData
 
 import json
 
 from djadmin_config.settings import DATA_FILES_ROOT
+
